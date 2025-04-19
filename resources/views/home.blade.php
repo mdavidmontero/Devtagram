3e824338a883903ef3d15248993e3e032c4d867f
Mateo Rodriguez <mdavidmontero6@gmail.com>
2025-04-19 10:50:36 -0500

    Agregando componente reutilizable para lista de publicaciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,23 +6,5 @@
 
 
 @section('contenido')
-    @if ($posts->count())
-        <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            @foreach ($posts as $post)
-                <div class="">
-                    <a href="{{ route('posts.show', ['post' => $post, 'user' => $post->user]) }}">
-                        <img src="{{ asset('uploads') . '/' . $post->imagen }}"
-                            alt="Imagen de la publicación {{ $post->titulo }}">
-                    </a>
-                </div>
-            @endforeach
-        </div>
-
-        <div class="my-10">
-            {{ $posts->links('pagination::tailwind') }}
-        </div>
-    @else
-        <p class="text-center">No hay posts, sigue a alguien para ver sus publicaciones</p>
-    @endif
-
+    <x-listar-post :posts="$posts" />
 @endsection
